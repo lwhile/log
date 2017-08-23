4442c08778faaaaf28e70b7180315a645162c33e
lwh <lwhile521@gmail.com>
2017-08-23 18:03:46 +0800

    更换单元测试的方式

--- log_test.go
+++ log_test.go
@@ -1,79 +1,84 @@
 package log
 
 import (
-	"fmt"
-	"os"
 	"testing"
 	"time"
-
-	"reflect"
 
 	"github.com/sirupsen/logrus"
 )
 
-var logPath = "log.log"
-
-func removeLogFile(fileName string) error {
-	return os.Remove(fileName)
-}
-
-func fileExist(path string) bool {
-	_, err := os.Stat(path)
-	if err != nil && os.IsNotExist(err) {
-		return false
-	}
-	if err != nil {
-		Error(err)
-	}
-	return true
-}
-
-func Test_convert2logrusLevels(t *testing.T) {
-	levels := []Level{InfoLevel, WarnLevel, ErrorLevel, FatalLevel}
-	lglels := convert2logrusLevels(levels...)
-
-	if reflect.TypeOf(lglels[0]) != reflect.TypeOf(logrus.InfoLevel) {
-		t.Fatal()
+func TestAddHooks(t *testing.T) {
+	allLevels := []logrus.Level{logrus.ErrorLevel, logrus.InfoLevel, logrus.PanicLevel, logrus.DebugLevel, logrus.WarnLevel, logrus.FatalLevel}
+	for _, ele := range allLevels {
+		if len(baseLogger.entry.Logger.Hooks[ele]) != 0 {
+			t.Fatalf("len(baseLogger.entry.Logger.Hooks[%s]) != 0\n", ele.String())
+		}
 	}
 
-	if reflect.TypeOf(lglels[1]) != reflect.TypeOf(logrus.WarnLevel) {
-		t.Fatal()
-	}
-
-	if reflect.TypeOf(lglels[2]) != reflect.TypeOf(logrus.FatalLevel) {
-		t.Fatal()
-	}
-}
-
-func Test_AddRotateHook(t *testing.T) {
-
-	err := AddRotateHook("log.log", InfoLevel, time.Second*5, time.Second*5, "%Y-%m-%d@%H:%M")
+	// test add a sentry hook
+	var sentryDSN = "http://634a78c491f54d2e9666e2ff36e0d747:1bd683754e9f4c38838e50f0b2b28d49@192.168.1.100:9000/2"
+	err := AddSentryHook(sentryDSN, InfoLevel, ErrorLevel)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	timePrefix := time.Now().Format("2006-01-02@15:04")
-	fileName := fmt.Sprintf("%s.%s.%s", logPath, "info", timePrefix)
+	if len(baseLogger.entry.Logger.Hooks[logrus.InfoLevel]) != 1 {
+		t.Fatal("len(baseLogger.entry.Logger.Hooks[logrus.InfoLevel]) != 1")
+	}
 
-	done := make(chan struct{})
+	if len(baseLogger.entry.Logger.Hooks[logrus.ErrorLevel]) != 1 {
+		t.Fatal("len(baseLogger.entry.Logger.Hooks[logrus.ErrorLevel]) != 1")
+	}
 
-	for {
-		select {
-		case <-done:
-			if !fileExist(fileName) {
-				t.Fatal("log file no exist")
-			}
-			removeLogFile(fileName)
-			return
-		default:
-			Info(testInfoLog)
-		}
-		time.Sleep(time.Millisecond * 500)
+	if len(baseLogger.entry.Logger.Hooks[logrus.FatalLevel]) != 0 {
+		t.Fatal("len(baseLogger.entry.Logger.Hooks[logrus.FatalLevel]) != 0")
+	}
+
+	// test add a rotate hook
+	err = AddRotateHook("log.log", InfoLevel, time.Second, time.Second, "")
+	if err != nil {
+		t.Fatal(err)
 	}
 
 }
 
-func 
+// var logPath = "log.log"
+
+// func removeLogFile(fileName string) error {
+// 	return os.Remove(fileName)
+// }
+
+// func fileExist(path string) bool {
+// 	_, err := os.Stat(path)
+// 	if err != nil && os.IsNotExist(err) {
+// 		return false
+// 	}
+// 	if err != nil {
+// 		Error(err)
+// 	}
+// 	return true
+// }
+
+// func Test_convert2logrusLevels(t *testing.T) {
+// 	levels := []Level{InfoLevel, WarnLevel, ErrorLevel, FatalLevel}
+// 	lglels := convert2logrusLevels(levels...)
+
+// 	if reflect.TypeOf(lglels[0]) != reflect.TypeOf(logrus.InfoLevel) {
+// 		t.Fatal()
+// 	}
+
+// 	if reflect.TypeOf(lglels[1]) != reflect.TypeOf(logrus.WarnLevel) {
+// 		t.Fatal()
+// 	}
+
+// 	if reflect.TypeOf(lglels[2]) != reflect.TypeOf(logrus.FatalLevel) {
+// 		t.Fatal()
+// 	}
+// }
+
+// func Test_AddRotateHook(t *testing.T) {
+
+// }
 
 // func Test_AddSentryHook(t *testing.T) {
 // 	var sentryDSN = "http://634a78c491f54d2e9666e2ff36e0d747:1bd683754e9f4c38838e50f0b2b28d49@192.168.1.100:9000/2"
@@ -83,7 +88,10 @@ func
 // 	}
 // 	Info(testInfoLog)
 // 	Error(testErrorLog)
+
+// 	hooks := baseLogger.entry.Logger.Hooks
+// 	fmt.Println(hooks[logrus.InfoLevel][0])
 // }
 
-var testInfoLog = "a test log of info level"
-var testErrorLog = "a test log of error level"
+// var testInfoLog = "a test log of info level"
+// var testErrorLog = "a test log of error level"
