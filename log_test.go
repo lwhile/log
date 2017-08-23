158f4ea21427236f0786aafb438c36fc69a75bc8
lwh <lwhile521@gmail.com>
2017-08-23 16:32:58 +0800

    补充两个函数convert2logrusLevels与函数AddSentryHook的单元测试

--- log_test.go
+++ log_test.go
@@ -1,31 +1,36 @@
-// Copyright 2015 The Prometheus Authors
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package log
 
-// func TestFileLineLogging(t *testing.T) {
-// 	var buf bytes.Buffer
-// 	origLogger.Out = &buf
-// 	origLogger.Formatter = &logrus.TextFormatter{
-// 		DisableColors: true,
-// 	}
+import (
+	"testing"
 
-// 	// The default logging level should be "info".
-// 	Debug("This debug-level line should not show up in the output.")
-// 	Infof("This %s-level line should show up in the output.", "info")
+	"reflect"
 
-// 	re := `^time="*" level=info msg="This info-level line should show up in the output." source="log_test.go:33" \n$`
-// 	if !regexp.MustCompile(re).Match(buf.Bytes()) {
-// 		t.Fatalf("%q did not match expected regex %q", buf.String(), re)
-// 	}
-// }
+	"github.com/sirupsen/logrus"
+)
+
+func Test_convert2logrusLevels(t *testing.T) {
+	levels := []Level{InfoLevel, WarnLevel, ErrorLevel, FatalLevel}
+	lglels := convert2logrusLevels(levels...)
+
+	if reflect.TypeOf(lglels[0]) != reflect.TypeOf(logrus.InfoLevel) {
+		t.Fatal()
+	}
+
+	if reflect.TypeOf(lglels[1]) != reflect.TypeOf(logrus.WarnLevel) {
+		t.Fatal()
+	}
+
+	if reflect.TypeOf(lglels[2]) != reflect.TypeOf(logrus.FatalLevel) {
+		t.Fatal()
+	}
+}
+
+func Test_AddSentryHook(t *testing.T) {
+	var sentryDSN = "http://634a78c491f54d2e9666e2ff36e0d747:1bd683754e9f4c38838e50f0b2b28d49@192.168.1.100:9000/2"
+	err := AddSentryHook(sentryDSN, InfoLevel, ErrorLevel)
+	if err != nil {
+		t.Fatal(err)
+	}
+	Info("a test log of info level")
+	Error("a test log of error level")
+}
